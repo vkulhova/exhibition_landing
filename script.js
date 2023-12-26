0c5ef53fd4601534719d0636f21e53c9310c4e96
Vladyslava Kulhova <vladyslava.kulhova@gmail.com>
2023-12-26 22:11:36 +0100

    add script

---
+++ script.js
@@ -0,0 +1,4 @@
+function togglePopup(index) {
+    const scheduleElement = document.getElementsByClassName('schedule-element')[index];
+    scheduleElement.classList.toggle("schedule-element-open")
+}
